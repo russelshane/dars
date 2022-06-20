5f6a40e583ad668e21d2c8cfd9783ac5cfc79542
darsler@pm.me <darsler@pm.me>
2022-06-20 18:13:25 +0800

    chore: prepare pages

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1>Hello, World</h1>
+    </div>
+  )
+}
+
+export default Home
